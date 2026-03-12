fc477d5b5927fdc1adc524f8870b1886a6f515a8
Marshzin <70143318+Marshzin@users.noreply.github.com>
2026-03-12 00:23:24 -0300

    Update automacao.py

--- automacao.py
+++ automacao.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from playwright.sync_api import sync_playwright
 
 
-HEADLESS = False
+HEADLESS = True
 
 
 def ler_dados(caminho="dados.txt"):
@@ -312,13 +312,14 @@ def executar_automacao(notify=None):
 
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=HEADLESS,
-            args=[
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-            ]
-        )
+    headless=True,
+    channel="chromium",
+    args=[
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+    ]
+)
 
         try:
             for i, usuario in enumerate(usuarios, start=1):
